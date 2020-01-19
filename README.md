c4e80f83cdf912ba231bf1644c3385c2949672c9
Navkiran Singh <singh.navkiran1515@gmail.com>
2020-01-19 20:07:42 +0530

    First version

---
+++ README.md
@@ -0,0 +1,6 @@
+# TOPSIS implementation in python for multi-criteria decision making
+
+## To use via command line
+`python3 run.py data.csv 25,25,25,25 -+++`
+
+Based off on a similar package for TOPSIS in R
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="navkiran", 
+    version="0.0.1",
+    author="Navkiran Singh",
+    author_email="nsingh2_be17@thapar.edu",
+    description="TOPSIS implementation in python for multi-criteria decision making",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/navkiran/topsis_project_navkiran",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
