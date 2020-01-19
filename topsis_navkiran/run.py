a4805e35ec78a2d1cb64bb1c23a7cc5dc8208c5d
Navkiran Singh <singh.navkiran1515@gmail.com>
2020-01-19 20:23:58 +0530

    Second version

--- topsis_navkiran/run.py
+++ topsis_navkiran/run.py
@@ -11,9 +11,10 @@ from topsis_navkiran import topsis
 
 arguments = sys.argv[1:]
 assert len(arguments) == 3, "Insufficient number of arguments provided, 3 required -> filename with extension, weights separated by comma and impact string"
-    
+
 filename = sys.argv[1]
 assert filename, "Filename must be provided"
+assert '.csv' in filename, "File extension must be .csv"
     
 data = pd.read_csv(filename,header=0,index_col=0)
 weights = sys.argv[2].split(',')
