c87a2d7f7b9aa74036c7e27fbaf34614e79ab7f0
Navkiran Singh <singh.navkiran1515@gmail.com>
2020-01-19 22:29:01 +0530

    1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="topsis_navkiran", 
-    version="1.0.1",
+    version="1.0.2",
     author="Navkiran Singh",
     author_email="nsingh2_be17@thapar.edu",
     description="TOPSIS implementation in python for multi-criteria decision making",
